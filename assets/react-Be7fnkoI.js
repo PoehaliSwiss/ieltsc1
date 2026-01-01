696f6d2f93a5e700118d4dc9c4685b929e6a4dc7
PoehaliSwiss <PoehaliSwiss@users.noreply.github.com>
2026-01-01 16:38:51 +0000

    deploy: 8326ab49108352991aabbc4e6b29981eaa6876f4

--- assets/react-Be7fnkoI.js
+++ assets/react-Be7fnkoI.js
@@ -1,4 +1,4 @@
-import{R as W}from"./index-Cnkf9B5q.js";const $=["abort","canplay","canplaythrough","durationchange","emptied","encrypted","ended","error","loadeddata","loadedmetadata","loadstart","pause","play","playing","progress","ratechange","seeked","seeking","stalled","suspend","timeupdate","volumechange","waiting","waitingforkey","resize","enterpictureinpicture","leavepictureinpicture","webkitbeginfullscreen","webkitendfullscreen","webkitpresentationmodechanged"],A=globalThis.document?.createElement("template");A&&(A.innerHTML=`
+import{R as W}from"./index-hoM6jB__.js";const $=["abort","canplay","canplaythrough","durationchange","emptied","encrypted","ended","error","loadeddata","loadedmetadata","loadstart","pause","play","playing","progress","ratechange","seeked","seeking","stalled","suspend","timeupdate","volumechange","waiting","waitingforkey","resize","enterpictureinpicture","leavepictureinpicture","webkitbeginfullscreen","webkitendfullscreen","webkitpresentationmodechanged"],A=globalThis.document?.createElement("template");A&&(A.innerHTML=`
     <style>
       :host {
         display: inline-block;
